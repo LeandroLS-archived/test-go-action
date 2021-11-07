b6dc181ffbb371945bb2293677250811fded1028
Leandro Lima <llimas@outlook.com>
2021-11-07 15:53:13 -0300

    changing action.yml

---
+++ mycustomaction.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Println("Olá")
+	// file := CreateFile()
+	// fmt.Printf(file.Name())
+}
+
+func CreateFile() *os.File {
+
+	file, err := os.Create("aaaa.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	return file
+}
